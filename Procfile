08f352902f680547bd3020b87efeb35810ed4ee5
kira-coding <103301025+kira-coding@users.noreply.github.com>
2023-01-14 02:54:58 -0800

    heroku setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn ToolShop.wsgi:application --log-file -
--- ToolShop/settings.py
+++ ToolShop/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_URL='/images/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
@@ -126,3 +129,4 @@ STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK="bootstrap4"
+django_heroku.settings(locals())
